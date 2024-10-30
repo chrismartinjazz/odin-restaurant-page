2e2d23b4aefe6e12f63a0b01d1440f32450bc06f
Chris Martin <chrismartinjazz@gmail.com>
2024-10-31 06:54:02 +1030

    Initial layout and logo

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./style.css"
+// import restaurantImage from "./restaurant.jpg";
+
+// const image = document.createElement("img");
+// image.src = restaurantImage;
+// document.body.appendChild(image);
+
+console.log("Test");
